fde90072e2158e3b490ac822b44e13bde81f0499
another-pw <poomrapee.war@student.mahidol.edu>
2024-01-16 14:20:00 +0700

    remove sqlite file from git tracking

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/user.input';
 
 @Injectable()
 export class UserService {
-  private readonly logger = new Logger(UserService.name);
-
   constructor(private prismaService: PrismaService) {}
 
-  async getAllUsers() {
-    const users = await this.prismaService.user.findMany();
-    this.logger.log(users);
-    return users;
+  getAllUsers() {
+    return this.prismaService.user.findMany();
   }
 
   getUserById(id: number) {
